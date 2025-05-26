67a3dbc361f3b7fba42d814ce866e5e58676bb0f
Andreas Rasmusson <andreasrasmusson1975@outlook.com>
2025-05-26 17:15:36 +0200

    Added evaluation folder

--- classes/task_generator.py
+++ classes/task_generator.py
@@ -1,12 +1,34 @@
+"""
+This module provides the TaskGenerator class for generating text extraction and record creation tasks
+(refer to text_extractor.py and record_creator.py for details) for the contents of a folder containing 
+subfolders with manual images (the docs folder - only visible after the installation script, install.bat, 
+has been run).
+"""
+
+# Perform necessary imports 
 from pathlib import Path
 
 class TaskGenerator:
+    """
+    A class for generating text extraction and record creation tasks
+    """
     def __init__(self,data_dir: str):
+        # Set the data directory and initialize the manual_folders list
         self.data_dir = Path(data_dir)
         self.manual_folders = sorted([f for f in self.data_dir.iterdir() if f.is_dir()])
         
-    def get_tasks(self):
+    def get_tasks(self) -> dict:
+        """
+        Creates and returns the tasks in the form of a dictionary.
+
+        Returns
+            dict: The dictionary of tasks
+
+        """
+        
         tasks={}
+        # Iterate over the paths in manual_folders and add tasks to the
+        # tasks dictionary.
         for manual_path in self.manual_folders:
             manual_path_str = str(manual_path)
             manual_name = manual_path.stem
